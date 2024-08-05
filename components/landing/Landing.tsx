6fe45b192820ac5598b035b028b540d13c2dcc11
Sharktooth <22bcs50186@cuchd.in>
2024-08-05 17:58:39 +0530

    pushing

--- components/landing/Landing.tsx
+++ components/landing/Landing.tsx
@@ -1,10 +1,20 @@
 import React from "react";
-import Spline from "./SplineLanding";
+import Spline from '@splinetool/react-spline/next';
+import Lamp from './Lamp'
 
 export default function Landing() {
   return (
-    <div className="h-screen w-full">
-      <Spline />
-    </div>
+    <>
+      <div className="relative flex justify-center items-center w-full h-screen">
+        <div className="z-10 absolute bg-black w-full h-screen ">
+          <Spline scene="https://prod.spline.design/wNyGL11HCHDC5yVC/scene.splinecode" />
+        </div>     
+        <div className= " z-20 absolute flex justify-center items-center bg w-full h-screen bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-5 ">
+       
+          
+        </div>  
+      </div>
+        
+    </>
   );
 }
