bfa874986bd0e9101449e014bd036005c46ea066
Satyansh Mittal <mittalsatyansh@gmail.com>
2024-08-07 03:19:52 +0530

    wallet connected

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
+//@ts-nocheck
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import './globals.css'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +17,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark" >
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="dark">
+      <body className={inter.className}>
+      <ThirdwebProvider desiredChainId={ChainId.Mumbai}>{children}
+      </ThirdwebProvider>
+      </body>
     </html>
   );
 }
